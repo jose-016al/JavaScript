8f7c7d1ca4d2d8dc01903d8164988ff98ac58189
Jose Almiron <jose_016al@outlook.com>
2022-07-17 17:13:33 +0200

    Change prueba.js

--- prueba.js
+++ prueba.js
@@ -1,1 +1,8 @@
-console.log("Hola Mundo");
+"use.strict";
+
+let numero = 37;
+console.log(typeof numero);
+let string_numero = String(numero);
+console.log(typeof string_numero);
+let lista = [];
+lista.unshift
