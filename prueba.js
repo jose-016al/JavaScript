94ed79db50079a4ce582aeb16cafa7ed4da4bde0
Jose Almiron <jose_016al@outlook.com>
2022-07-23 13:36:05 +0200

    Change prueba.js

--- prueba.js
+++ prueba.js
@@ -6,3 +6,10 @@ let string_numero = String(numero);
 console.log(typeof string_numero);
 let lista = [];
 lista.unshift
+
+let nombre = prompt("¿Como te llamas?", "nombre");
+nombre = nombre ? nombre : "anonimo";
+
+let edad = prompt("Hola " + nombre + " ¿cuantos años tienes?");
+alert(nombre + " tiene " + edad + " años");
+
