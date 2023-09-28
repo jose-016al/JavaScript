0b73cb68b6d4e9ad459b4c02271f9abbd1b1d904
jose-016al <jose_016al@outlook.com>
2023-09-28 17:05:21 +0200

    updates

--- Practicas/Ejercicio sobre arrays/script.js
+++ Practicas/Ejercicio sobre arrays/script.js
@@ -1,29 +1,57 @@
 /* VARIABLES */
 var arr1 = [1, 1, 2, 2];
 var arr2 = [5, 5, 5, 5];
-var arr = ["hola", "hola", "adios", "bienvenido", "adios"];
+var arr = [6, 8, 20, 8, 4, 22, 88, 4333, 22, 1, 1, 4, 20, 1, 29, 827463, 21, 43, 9]
+var text = "Hola me llamo jose, ¿que tal?. Jose, hola estoy bien, yo me llamo Javi";
 
-/* FUNCIONES */
-/* Escribe una función llamada sumaArrays que tome dos arrays arr1 y arr2 del mismo tamaño.
-La función debe devolver un nuevo array que contenga la suma de los elementos
-correspondientes de arr1 y arr2 . */
-const sumaArrays = (arr1, arr2) => {
-    arr1.length == arr2.length ? arr1.map((item, index) => item + arr2[index]) : "Los arrays no tienen el mismo tamaño"
+/* Ejercicio 1 */
+const sumaArrays = (arr1, arr2) => arr1.length == arr2.length ? arr1.map((item, index) => item + arr2[index]) : "Los arrays no tienen el mismo tamaño";
+
+/* Ejercicio 2 */
+const eliminarDuplicados = arr => arr.filter((item, index) => arr.indexOf(item) === index);
+
+/* Ejercicio 3 */
+const filtrarPares = arr => arr.filter(item => item % 2 == 0);
+
+/* Ejercicio 4 */
+const unirArrays = (...arrays) => arrays.reduce((accumulated, currentValue) => accumulated.concat(currentValue));
+
+/* Ejercicio 5 */
+const contarPalabras = text => {
+    const count = {};
+    text = text.toLowerCase();
+    text.split(/[ ,.]+/).map(word  => !count[word] ? count[word] = 1 : count[word]++);
+    return JSON.stringify(count);
 }
 
-/* Define una función llamada eliminarDuplicados que tome un array arr . La función debe
-eliminar los elementos duplicados y devolver un nuevo array con elementos únicos, manteniendo
-el orden original. */
-const eliminarDuplicados = arr => arr.filter((item, index) => arr.indexOf(item) === index)
+/* Ejercicio 6 */
+const ordenarNumeros = arr => arr.sort((a, b) => b - a);
 
-/* Crea una función llamada filtrarPares que tome un array de números arr y devuelva un
-nuevo array que contenga solo los números pares. */
-const filtrarPares = arr1 => arr1.filter(item => item % 2 == 0)
+/* Ejercicio 7 */
+const eliminarElemento = (arr, element) => arr.indexOf(element) !== -1 && arr.splice(arr.indexOf(element), 1) && arr;
 
-/* Escribe una función llamada unirArrays que acepte un número variable de arrays y los combine
-en uno solo. Nota: Se podría utilizar el método reduce y el método concat para lograrlo. */
-const unirArrays = (...arrays) => {
-    return arrays.reduce((accumulated, currentValue) => accumulated.concat(currentValue));
-} 
+/* Ejercicio 8 */
+const encontrarMaxMin = arr => JSON.stringify({max: Math.max(...arr), min: Math.min(...arr)});
 
-console.log(unirArrays(arr1, arr2));
+/* Ejercicio 9 */
+const buscarElemento = (arr, element) => arr.indexOf(element) !== -1 ? arr.indexOf(element) : -1;
+
+/* Ejercicio 10 */
+const dividirFragmento = (arr, num) => {
+    const fragmentSize = Math.ceil(arr.length / num);
+    return arr.map((item, index) =>
+      `[${arr.slice(index * fragmentSize, (index + 1) * fragmentSize)}]`
+    ).filter(arr => arr !== '[]');
+}
+
+/* OUT */
+console.log(`Ejercicio 1: Suma de Arrays [${sumaArrays(arr1, arr2)}]`);
+console.log(`Ejercicio 2: Duplicados [${eliminarDuplicados(arr)}]`);
+console.log(`Ejercicio 3: Filtrar Pares [${filtrarPares(arr)}]`);
+console.log(`Ejercicio 4: Union de Arrays [${unirArrays(arr, arr1, arr2)}]`);
+console.log(`Ejercicio 5: Conteo de Palabras ${contarPalabras(text)}`);
+console.log(`Ejercicio 6: Ordenar Numeros [${ordenarNumeros(arr)}]`);
+console.log(`Ejercicio 7: Eliminar Elementos [${eliminarElemento(arr, 1)}]`);
+console.log(`Ejercicio 8: Maximo y Minimo ${encontrarMaxMin(arr)}`);
+console.log(`Ejercicio 9: Buscar Elemento ${buscarElemento(arr, 20)}`);
+console.log(`Ejercicio 10: Dividir en Fragmentos [${dividirFragmento(arr, 8)}]`);
